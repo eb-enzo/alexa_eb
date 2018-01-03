11202dd03905f37cc374236a3233a4da086ae029
Enzo Cappa <enzo@eventbrite.com>
2018-01-03 14:52:20 -0300

    more work

--- eventbrite.py
+++ eventbrite.py
@@ -6,7 +6,7 @@ import requests
 from flask import Flask, render_template
 from flask_ask import Ask, question, session, statement
 
-TOKEN = 'CYFOVAWPYPSBXVEJFQ6O'
+TOKEN = '7FLR77ARPTR4VLY3JFMU'
 
 app = Flask(__name__)
 
@@ -23,40 +23,39 @@ def new_game():
 
 @ask.intent("EventsInMyAreaIntent", convert={'when': str, })
 def answer(when):
+    event = call_eb_api(None)
+    result = statement('This event is happening in your area').simple_card(**event)
 
-    msg = render_template('events')
-
-    return statement(call_eb_api(None))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return result
 
 
 def call_eb_api(when):
 
     response = requests.post(
-        'https://www.eventbriteapi.com/v3/destination/search/?token={}'.format(TOKEN),
-        data={
+        'https://www.evbqaapi.com/v3/destination/search/?token={}'.format(TOKEN),
+        data=json.dumps({
             "event_search": {
-
                 "dates": [
                     "current_future"
                 ],
-                "page_size": 10,
+                "page_size": 1,
+                "page_number": 1,
                 "point_radius": {
                     "latitude": -32.895601,
                     "radius": "10km",
                     "longitude": -68.834241
                 }
             }
-        }
+        }),
+        headers={'content-type': 'application/json'},
     )
     parsed_response = json.loads(response.text)
+    e = parsed_response['events']['results'][0]
 
-    event_descriptions = [
-        e.name for e in parsed_response['result']['events']
-    ]
+    event_description = {'title': e['name'], 'content': e['summary']}
 
-    logging.getLogger("flask_ask").debug(event_descriptions)
-    return event_descriptions
+    return event_description
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
